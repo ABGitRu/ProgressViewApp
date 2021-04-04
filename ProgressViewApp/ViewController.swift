825181e8ad035616829a875e256ded3a293f5ecb
Alexey Barinov <begoodbebad2012@gmail.com>
2021-04-04 14:22:09 +0300

    Added

--- ProgressViewApp/ViewController.swift
+++ ProgressViewApp/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var screenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        screenView.layer.cornerRadius = 20
+        screenView.isHidden = true
+        
+        progressView.progressTintColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        progressView.progress = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if self.progressView.progress < 1 {
+                self.progressView.progress += 0.2
+            } else {
+                self.progressView.isHidden = true
+                self.screenView.isHidden = false
+            }
+        }
     }
+    
+    
 
 
 }
